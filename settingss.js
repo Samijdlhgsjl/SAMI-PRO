657f3859bc944d5a8fa5a31cbf2722e76a10ad5a
Samijdlhgsjl <118826751+Samijdlhgsjl@users.noreply.github.com>
2025-08-30 14:33:49 +0300

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~h3tCHarS#75szxFckAhqgI-TAni2MO6w07iATLdWuBo901Bvru9E",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "256766104887", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
